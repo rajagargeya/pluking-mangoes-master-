f39733463191e6dc4c36c7e2a081549658a4e24e
rajagargeya <74851472+rajagargeya@users.noreply.github.com>
2021-04-06 12:48:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ const Render = Matter.Render;
 const Constraint=Matter.Constraint;
 var treeObj, stoneObj,groundObject, launcherObject;
 var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
-var world,boy;
+var World,boy;
 var launchingForce=100;
 
 function preload(){
